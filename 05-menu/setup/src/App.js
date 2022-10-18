71943fa23001976acb624f8e65722df26045b99f
dibdas <dibyandu08das@gmail.com>
2022-10-19 01:29:07 +0530

    modified

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,7 +4,25 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const[menu,setMenu]= useState(items)
+  const[categories,setCategories] = useState([])
+
+  return (
+  
+  <main>
+    <h2>menu project setup</h2>
+    <section className='menu-section'>
+      <div className='title'>
+        <h2>Our menu</h2>
+        <div className='underline'></div>
+      </div>
+      <Categories />
+      <Menu items={items}/>
+
+    </section>
+
+  </main>
+  )
 }
 
 export default App;
