2bf8127901c25db3f4e0904fc25f60854be1e5c9
dibdas <dibyandu08das@gmail.com>
2022-10-18 23:51:52 +0530

    modified

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const[questions,setQuestions] = useState(data)
+  return (
+    <article className='container'>
+      <h2>accordion project setup</h2>
+      <section className='info'>
+        {questions.map((question)=>{
+          const{id} = question
+          return (<SingleQuestion key={id} {...question} />)
+
+        })}
+      </section>
+    </article>
+  )
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title,info}) => {
+  const[showinfo,setShowInfo] = useState(false)
+  return (
+  <article className='question'>
+    <h2>question component</h2>
+    <header>
+      <h4>{title}</h4>
+      {/* my code d.d
+      <button onClick={()=>setShowInfo(!showinfo)} className='btn'>{showinfo?<AiOutlineMinus/>:<AiOutlinePlus />}</button>
+    
+  <p>{showinfo? info.substring(0,300):info.substring(0,30)}</p> */}
+    <button className='btn' onClick={()=>setShowInfo(!showinfo)}>
+      {showinfo ? <AiOutlineMinus/>: <AiOutlinePlus/>}</button> 
+    
+  </header>
+  {showinfo && <p>{info}</p>}
+  </article>
+  )
 };
 
 export default Question;
