d0b1293d754cedafd2161651865032bb6e347334
dibdas <dibyandu08das@gmail.com>
2022-10-20 02:11:54 +0530

    modified

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,7 +3,30 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const[people,setPeople] = useState(data)
+  const [index,setIndex] = useState(0)
+  return (
+    <section className='section'>
+        <h3>slider project setup</h3>
+      <div className='title'>
+        <h2><span>/</span>reviews</h2>
+      </div>
+      <div className='section-center'>
+        {people.map((person,index)=>{
+          const{name,id,image,title,quote} = person
+          return(
+            <article key={id}>
+              <img />
+
+            </article>
+
+          )
+
+        })}
+      </div>
+  
+    </section>
+  )
 }
 
 export default App;
