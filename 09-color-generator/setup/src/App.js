045d4573aa02abb9bbc752b44ac0854186a914be
dibdas <dibyandu08das@gmail.com>
2022-10-21 15:24:24 +0530

    modified

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,61 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+
+  const [colors,setColors] = useState('')
+  const [error,setError] = useState(false)
+  // const [list,setList] = useState([])
+  // passing the default value before the use provide any input 
+  
+  // watch the last leg of the video to get this it can be all(10) or all(20)
+   const [list,setList] = useState(new Values('#f15025').all(10))
+
+  const handleSubmit=(event)=>{
+    event.preventDefault()
+    console.log("ht")
+  
+    
+    try{
+      let colours = new Values(colors).all(10)
+      console.log(colours)
+      setList(colours)
+
+    }
+    catch(error){
+      setError(true)
+      console.log(error)
+
+    }
+  }
+
+  return (
+    <>
+    <section className='container'>
+      <h3>color generator project</h3>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        value={colors} 
+        onChange={(event)=>setColors(event.target.value)}
+        // if the error state value is true, then the error class or no class i.e null 
+        placeholder="#f15025" className={`${error?'error' :null}`}/>
+        <button type='submit' className='btn'>submit</button>
+      </form>
+    </section>
+    <section className='colors'>
+      <h4>list goes here</h4>
+      {list.map((coloritems,index)=>{
+        return(
+          // {...coloritems} it is object spread operator 
+    
+          <SingleColor key={index} {...coloritems} 
+          index={index} hexColor={coloritems.hex}/>
+        )
+
+      })}
+    </section>
+    </>
+    )
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb,weight,index,hexColor}) => {
+  const[alert,setAlert] = useState(false)
+  const bcg = rgb.join(',')
+  console.log("rgb   "+ rgb)
+  console.log(bcg)
+  const hex= rgbToHex(...rgb)
+  const hexvalue = `#${hexColor}`
+  console.log("hexColor  "+  hexColor +"  "+ "index"+  index) // getting the hex color  from th library
+  
+  // to disappear the cpliboad after seconds 
+  useEffect(()=>{
+   const timeOut = setTimeout(() => {
+    setAlert(false)
+   },5000)
+   // before setup another time out need to clear the first timeout , hence used clean up function
+   return ()=>clearTimeout(timeOut) // using the clean up function 
+  },[alert])
+
+  return (
+ 
+    <article 
+    className={`color ${index>10 && 'color-light'}`}
+    onClick={()=>{setAlert(true) 
+      navigator.clipboard.writeText(hexvalue)}}
+
+    style={{background:`rgb(${bcg})`}}>
+     {' '}
+     <p className='percent-value'>{weight}%</p>
+     <p className='color-value'>{hex}%</p>
+     {/* oR get the color from the library */}
+     {/*<p className='color-value'>{hexColor}</p> */}
+     <p className='color-value'>{hexvalue}%</p>
+
+     {alert && <p className='alert'>copy to clipboard</p>}
+     
+     <h4>single color component</h4>
+    </article>
+  )
 }
 
 export default SingleColor
