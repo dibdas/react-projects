db13e72f78736c50811d655173be86455c643049
dibdas <you@example.com>
2022-10-23 19:59:43 +0530

    modified

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,67 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+
+  const[showLinks,setShowLinks] = useState(false)
+    return (
+      <nav>
+        <div className="nav-center">
+          <div className='nav-header'>
+            <img src={logo} />
+            <button className='nav-toggle' onClick={()=>setShowLinks(!showLinks)}>
+              <FaBars />
+            </button>
+          </div>
+{/* 
+          {showLinks && (
+
+          <div className='links-container show-container'>
+            <ul className='links'>
+            {links.map((link)=>{
+              const{id,url,text} = link
+              return(
+                <li key={id}><a href={url}>{text}</a></li>
+              )
+            })
+            }
+            </ul>
+          </div>
+          )}
+
+
+ */}
+
+          <div className={`${showLinks ? 
+           'link-container show container' :'links-container'}`}>
+                      <ul className='links'>
+                      {links.map((link)=>{
+                        const{id,url,text} = link
+                        return(
+                          <li key={id}><a href={url}>{text}</a></li>
+                        )
+                      })
+                      }
+                      </ul>
+                    </div>
+                  
+
+
+
+          
+          <ul className='social-links'>
+            {social.map((soci)=>{
+              const{id,url,icon} = soci
+              return(
+                <li key={id} href={url}>{icon}</li>
+              )
+            })}
+          </ul>
+        </div>
+
+          
+      </nav>
+    );
+
 }
 
 export default Navbar
