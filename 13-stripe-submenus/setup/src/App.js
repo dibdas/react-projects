4d793501d9bdc6e40cca22bd79f56e3d7630d52d
dibdas <dibyandu08das@gmail.com>
2022-10-25 15:34:39 +0530

    modified

--- 13-stripe-submenus/setup/src/App.js
+++ 13-stripe-submenus/setup/src/App.js
@@ -3,10 +3,15 @@ import Navbar from './Navbar'
 import Hero from './Hero'
 import Sidebar from './Sidebar'
 import Submenu from './Submenu'
+
 function App() {
   return (
     <>
       <h2>stripe submenus setup</h2>
+      <Navbar/>
+      <Hero/>
+      <Sidebar/>
+      <Submenu/>
     </>
   )
 }
