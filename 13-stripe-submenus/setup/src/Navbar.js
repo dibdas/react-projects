0a1844e2f48c2f3c287f6a0839aba7cb089a8121
dibdas <dibyandu08das@gmail.com>
2022-10-26 00:16:11 +0530

    modified

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -5,8 +5,28 @@ import { useGlobalContext } from './context'
 
 const Navbar = () => {
   const{openSubmenu,openSidebar,closeSubmenu} = useGlobalContext()
+  const displaySubMenu =(event)=>{
+    console.log(event.target) 
+    const page = event.target.textContent
+    console.log(page)
+    const templeteBtn = event.target.getBoundingClientRect()
+    console.log(templeteBtn)
+
+    const center = ((templeteBtn.left+templeteBtn.right)/2)
+    const bottom = templeteBtn.buttom - 3 // moving the submenu 3px up
+  
+    openSubmenu(page,{center,bottom})
+  }
+
+  const handleSubMenu=(event)=>{
+    // when mouse is hovered around the navbar the submenu will be closed
+    if(!event.target.classList.contains("link-btn")){
+      closeSubmenu()
+    }
+  }
+  
   return (
-    <nav className='nav'>
+    <nav className='nav' onMouseOver={handleSubMenu}>
       <h5>navbar component</h5>
       <div className='nav-center'>
         <div className='nav-header'>
@@ -18,15 +38,15 @@ const Navbar = () => {
         </div>
         <ul className='nav-links'>
           <li>
-            <button className='link-btn'>products</button>
+            <button className='link-btn' onMouseOver={displaySubMenu}>products</button>
           </li>
 
           <li>
-            <button className='link-btn'>developers</button>
+            <button className='link-btn' onMouseOver={displaySubMenu}>developers</button>
           </li>
 
           <li>
-            <button className='link-btn'>company</button>
+            <button className='link-btn' onMouseOver={displaySubMenu}>company</button>
           </li>
 
         </ul>
