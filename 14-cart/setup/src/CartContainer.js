189343888f9cab44b9c72e80aa9bfd36b0d17528
dibdas <dibyandu08das@gmail.com>
2022-10-28 01:04:40 +0530

    modified

--- 14-cart/setup/src/CartContainer.js
+++ 14-cart/setup/src/CartContainer.js
@@ -3,7 +3,9 @@ import CartItem from './CartItem'
 import { useGlobalContext } from './context'
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext()
+  const { cart,total,clearCart } = useGlobalContext()
+  console.log(cart)
+  console.log(total)
   if (cart.length === 0) {
     return (
       <section className='cart'>
@@ -32,12 +34,14 @@ const CartContainer = () => {
         <hr />
         <div className='cart-total'>
           <h4>
-            total <span>$0.00</span>
+            {/* total <span>$0.00</span> */}
+            total <span>${total}</span>
           </h4>
         </div>
         <button
           className='btn clear-btn'
-          onClick={() => console.log('clear cart')}
+          // onClick={() => console.log('clear cart')}
+          onClick={clearCart}
         >
           clear cart
         </button>
