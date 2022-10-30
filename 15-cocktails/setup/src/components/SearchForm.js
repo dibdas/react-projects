0cea9f6a62c829de21dc504431fcf2bb25fedf7f
dibdas <dibyandu08das@gmail.com>
2022-10-30 21:47:59 +0530

    modified

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React,{useEffect, useRef} from 'react'
 import { useGlobalContext } from '../context'
 
+
+
 const SearchForm = () => {
+  const {setSearchTerm} = useGlobalContext()
+  const searchValue = useRef()
+   // using useRef providing the uncontrolled input
+  useEffect(()=>{
+    searchValue.current.focus()
+
+  },[]) // calling the useEffect only when the component renders , not everytime
+
+  const searchCocktails=()=>{
+   setSearchTerm(searchValue.current.value)
+  }
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+  }
+  
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <h5>search form component</h5>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label className='' htmlFor='name'>search for you favoiurite cocktails </label>
+          <input type="text" id="name" name="name"
+          ref={searchValue} onChange={searchCocktails}/>
+        </div>
+
+      </form>
+    </section>
   )
 }
 
